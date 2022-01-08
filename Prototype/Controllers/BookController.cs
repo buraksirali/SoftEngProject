c4d4d7f3e2730e60356b62c4affbdb2568d7372f
Burak Sirali <burak.sirali@piworks.net>
2022-01-08 17:41:38 +0300

    feat: add bookcontroller

--- Prototype/Controllers/BookController.cs
+++ Prototype/Controllers/BookController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Prototype.Data;
+using System.Collections.Generic;
 
 namespace Prototype.Controllers
 {
     public class BookController : Controller
     {
-        public IActionResult Index(string ISBN)
+        private readonly ApplicationDbContext _db;
+        private readonly ModelFactory modelFactory;
+
+        public BookController(ApplicationDbContext db)
         {
-            
+            _db = db;
+            modelFactory = new ModelFactory(_db);
+        }
+
+        public IActionResult Index(int id)
+        {
+            modelFactory.GetBook(id);
+
+            // Pdf To Text
+            /*Dictionary<string, string> data = new Dictionary<string, string>()
+            {
+                text: "",
+                name: ""
+            }*/
+
             return View();
         }
     }
